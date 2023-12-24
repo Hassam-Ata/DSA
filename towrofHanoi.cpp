3f67ea71116cd78f084b93add603fefbefd8d87e
Hassam-Ata <hassamata86@gmail.com>
2023-12-24 21:20:14 +0500

    changed parameters

--- towrofHanoi.cpp
+++ towrofHanoi.cpp
@@ -3,16 +3,16 @@
 #include <bits/stdc++.h> 
 using namespace std; 
 
-void towerOfHanoi(int n, char from_rod, char to_rod, 
-				char aux_rod) 
+void towerOfHanoi(int n, char A, char B, 
+				char C) 
 { 
 	if (n == 0) { 
 		return; 
 	} 
-	towerOfHanoi(n - 1, from_rod, aux_rod, to_rod); 
-	cout << "Move disk " << n << " from rod " << from_rod 
-		<< " to rod " << to_rod << endl; 
-	towerOfHanoi(n - 1, aux_rod, to_rod, from_rod); 
+	towerOfHanoi(n - 1, A, C, B); 
+	cout << "Move disk " << n << " from rod " << A 
+		<< " to rod " << C << endl; 
+	towerOfHanoi(n - 1, B, A, C); 
 } 
 
 // Driver code 
@@ -21,7 +21,7 @@ int main()
 	int N = 3; 
 
 	// A, B and C are names of rods 
-	towerOfHanoi(N, 'A', 'C', 'B'); 
+	towerOfHanoi(N, 'A', 'B', 'C'); 
 	return 0; 
 } 
 
